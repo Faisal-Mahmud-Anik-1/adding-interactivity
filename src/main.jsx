644393a3214577a8fcb159de2bbe8669897bb6c1
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2023-04-09 14:45:41 +0600

    ✔ Commit and Render

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/state-component-memory/App";
 import "./index.css";
+import App from "./render-and-commit/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
